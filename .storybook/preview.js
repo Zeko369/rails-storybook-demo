11ea8cb85c23fb11f9bf62c76c01e4a78de78b13
Fran Zekan <zekan.fran369@gmail.com>
2023-01-03 16:46:37 +0100

    Maybe fix?

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,10 @@
+let server = `http://localhost:3000/storybook`;
+if (process.env.NODE_ENV === "production") {
+  server = `https://web-production-1eef.up.railway.app/storybook`;
+}
+
 export const parameters = {
   server: {
-    url: `http://localhost:3000/storybook`,
+    url: server,
   },
 };
