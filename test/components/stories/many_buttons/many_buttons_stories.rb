29b1b6d03be3a2d1bae0b8e214d03f50ae29c73f
Fran Zekan <zekan.fran369@gmail.com>
2023-01-03 13:19:01 +0100

    Example with numbers

--- test/components/stories/many_buttons/many_buttons_stories.rb
+++ test/components/stories/many_buttons/many_buttons_stories.rb
@@ -2,5 +2,8 @@ class ManyButtons::ManyButtonsStories < ActionView::Storybook::Stories
   self.title = "Many Buttons"
 
   story(:default) do
+    controls do
+      number(:count, 5, min: 1, max: 15)
+    end
   end
 end
