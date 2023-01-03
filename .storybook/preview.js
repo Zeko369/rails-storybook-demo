eb6bf3f7285a69199bacc049e3c6a79ac31392bf
Fran Zekan <zekan.fran369@gmail.com>
2023-01-03 17:14:22 +0100

    Fix prod path and origin

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 let server = `http://localhost:3000/storybook`;
-if (!!process.env.RAILWAY_ENVIRONMENT) {
-  server = `https://web-production-1eef.up.railway.app/storybook`;
+if (window.location.protocol === "https:") {
+  server = `${window.location.origin}/storybook`;
 }
 
 export const parameters = {
