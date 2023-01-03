ff5d7b2471a8295f19c8ee16fb989986feeddd5c
Fran Zekan <zekan.fran369@gmail.com>
2023-01-03 16:26:23 +0100

    Fix storybook build

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-export NODE_OPTIONS=--openssl-legacy-provider
+bundle exec rake storybook_rails:write_stories_json
 
+export NODE_OPTIONS=--openssl-legacy-provider
 yarn build-storybook  -o ./public/docs
+
 mv ./public/docs/iframe.html ./public
