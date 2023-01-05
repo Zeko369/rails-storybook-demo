587de7879879e267aae580ffa05484755a585e2d
Fran Zekan <zekan.fran369@gmail.com>
2023-01-05 12:53:54 +0100

    Smarter way of doing asset precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = "1.0"
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += Dir[Rails.root.join("app/assets/stylesheets/**/*")].map { |f| f.split("/").last.split(".").first }.uniq
+# Rails.application.config.assets.precompile += Dir[Rails.root.join("app/assets/stylesheets/**/*")].map { |f| f.split("/").last.split(".").first }.uniq
