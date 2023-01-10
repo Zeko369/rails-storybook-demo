9da98ba981badc8e31d6c0bec2aa3c824491d1b5
Fran Zekan <zekan.fran369@gmail.com>
2023-01-10 10:47:03 +0100

    Fix some configs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += Dir[Rails.root.join("app/assets/stylesheets/**/*")].map { |f| f.split("/").last.split(".").first }.uniq
